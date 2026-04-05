0add1ca205d2ff4b21044bc304dfe571073d34bf
Omar Akharaz <omar.akharaz@gmail.com>
2026-04-05 19:09:58 +0200

    test: verify zenoh publication in test_video_devices_put

--- src/com/service.rs
+++ src/com/service.rs
@@ -18,11 +18,36 @@ impl Service {
 
 #[cfg(test)]
 mod tests {
+    use std::time::Duration;
+
     use super::*;
+    use zenoh::handlers::FifoChannel;
 
     #[tokio::test]
     async fn test_service_creation() {
         let service = Service::new().await;
         assert!(service.is_ok());
+    }
+
+    #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
+    async fn test_video_devices_put() {
+        let service = Service::new().await.unwrap();
+        let mut subscriber = service
+            .session
+            .declare_subscriber("video/devices")
+            .with(FifoChannel::new(1))
+            .await
+            .unwrap();
+
+        let data = vec![1, 2, 3, 4];
+        service.video_devices_put(data.clone()).await.unwrap();
+
+        let sample = tokio::time::timeout(Duration::from_secs(2), subscriber.recv_async())
+            .await
+            .expect("Timed out waiting for zenoh publication")
+            .unwrap();
+        assert_eq!(sample.payload().to_bytes().as_ref(), data.as_slice());
+
+        subscriber.undeclare().await.unwrap();
     }
 }

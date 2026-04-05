e0e637878d37366fbd03e3d4dbb810dacd7602d8
Omar Akharaz <omar.akharaz@gmail.com>
2026-04-05 19:09:58 +0200

    Insert decodebin and dynamic pad linking between capsfilter and videoconvert

--- src/video/streamer.rs
+++ src/video/streamer.rs
@@ -152,12 +152,12 @@ impl Streamer {
             .name(CAPSFILTER)
             .build()
             .map_err(|e| anyhow!("Failed to create capsfilter: {}", e))?;
-        capfilter.set_property("caps", gst::Caps::builder("video/x-raw")
-            .field("width", 640)
-            .field("height", 480)
+
+        let decobin = gst::ElementFactory::make("decodebin")
             .build()
-            .to_value()); // "image/jpeg", 1920, 1080
-        let convert = gst::ElementFactory::make("videoconvert")
+            .map_err(|e| anyhow!("Failed to create decodebin: {}", e))?;
+
+        let videoconvert = gst::ElementFactory::make("videoconvert")
             .build()
             .map_err(|e| anyhow!("Failed to create videoconvert: {}", e))?;
 
@@ -166,10 +166,38 @@ impl Streamer {
             .map_err(|e| anyhow!("Failed to create autovideosink: {}", e))?;
 
         // Add elements to pipeline
-        pipeline.add_many([&source, &capfilter, &convert, &sink])?;
+        pipeline.add_many([&source, &capfilter, &decobin, &videoconvert, &sink])?;
 
         // Link elements
-        gst::Element::link_many([&source, &capfilter, &convert, &sink])?;
+        gst::Element::link_many([&source, &capfilter, &decobin])?;
+        gst::Element::link_many([&videoconvert, &sink])?;
+
+        let videoconvert_weak = videoconvert.downgrade();
+        decobin.connect_pad_added(move |_dbin, src_pad| {
+            let Some(videoconvert) = videoconvert_weak.upgrade() else {
+                return;
+            };
+            let sink_pad = videoconvert.static_pad("sink").expect("Failed to get sink pad from videoconvert");
+            if sink_pad.is_linked() {
+                eprintln!("Sink pad already linked, ignoring");
+                return;
+            }
+
+            let src_pad_caps = src_pad.current_caps().or_else(|| Some(src_pad.query_caps(None)));
+            let Some(src_pad_caps) = src_pad_caps else {
+                eprintln!("Failed to get caps from src pad");
+                return;
+            };
+            let Some(src_pad_struct) = src_pad_caps.structure(0) else {return;};
+            let src_pad_type = src_pad_struct.name();
+
+            if src_pad_type.starts_with("video/") {
+                match src_pad.link(&sink_pad) {
+                    Ok(_) => eprintln!("Linked decodebin src pad to videoconvert sink pad"),
+                    Err(e) => eprintln!("Failed to link decodebin src pad to videoconvert sink pad: {}", e),
+                }
+            }
+        });
 
         Ok(pipeline)
     }
